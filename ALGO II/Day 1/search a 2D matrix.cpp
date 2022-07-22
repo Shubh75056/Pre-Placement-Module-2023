58c00ff31f20ee0786d089101654731193e3b803
SHUBHAM YADAV <108691486+Shubh75056@users.noreply.github.com>
2022-07-22 23:41:56 +0530

    create 2D matrix

---
+++ ALGO II/Day 1/search a 2D matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // start our "pointer" in the bottom-left
+        int row = matrix.size()-1;
+        int col = 0;
+
+        while (row >= 0 && col < matrix[0].size()) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else { // found it
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
