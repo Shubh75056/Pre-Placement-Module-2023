927fb70ba19bcb97c3c934e2c4d5937db870dee4
SHUBHAM YADAV <108691486+Shubh75056@users.noreply.github.com>
2022-07-23 16:19:44 +0530

    create peak element

---
+++ ALGO II/Day 2/find peak element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        int mid;
+
+        while(low < high) {
+            mid = low + (high - low)/2;
+            if(nums[mid] > nums[mid + 1]){
+                high = mid;
+            } else if(nums[mid] <= nums[mid + 1]){
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};
